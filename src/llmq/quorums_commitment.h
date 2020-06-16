10e92874c0ce0791bb33b04b1adca1ce6b36c113
Cofresi <cofresi@dash.org>
2020-06-16 14:17:27 -0400

    validMembersCountHex

--- src/llmq/quorums_commitment.h
+++ src/llmq/quorums_commitment.h
@@ -109,6 +109,7 @@ public:
         obj.push_back(Pair("signersCount", CountSigners()));
         obj.push_back(Pair("signers", CLLMQUtils::ToHexStr(signers)));
         obj.push_back(Pair("validMembersCount", CountValidMembers()));
+        obj.push_back(Pair("validMembersCountHex", CLLMQUtils::ToHexStr(CCompactSize(CountValidMembers()))));
         obj.push_back(Pair("validMembers", CLLMQUtils::ToHexStr(validMembers)));
         obj.push_back(Pair("quorumPublicKey", quorumPublicKey.ToString()));
         obj.push_back(Pair("quorumVvecHash", quorumVvecHash.ToString()));
