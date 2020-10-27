7290e9eca01cb7e87dad573498f540a4e48f884f
Cofresi <cofresi@dash.org>
2020-10-27 11:11:34 -0400

    print signHash

--- src/llmq/quorums_signing.cpp
+++ src/llmq/quorums_signing.cpp
@@ -981,6 +981,10 @@ CQuorumCPtr CSigningManager::SelectQuorumForSigningFixed(Consensus::LLMQType llm
     for (size_t i = 0; i < scores.size(); i++) {
         LogPrintf("sorted score -- %s\n", scores[i].first.ToString());
     }
+    ++++
+    clBlock = chainActive[signHeight];
+    uint256 signHash = CLLMQUtils::BuildSignHash(llmqType, quorums[scores.front().second]->qc.quorumHash, selectionHash, clBlock->GetBlockHash());
+    LogPrintf("signHash -- %s\n", signHash.ToString());
     return quorums[scores.front().second];
 }
 
